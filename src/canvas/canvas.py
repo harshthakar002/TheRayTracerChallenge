441ae2aa67c1474eb06c9d37b5ac997d170bf286
Harsh Thakar <harshthakar002@gmail.com>
2021-10-19 23:25:38 +0100

    [Canvas] Reading and writing pixels

--- src/canvas/canvas.py
+++ src/canvas/canvas.py
@@ -5,3 +5,9 @@ class canvas():
         self.width = width
         self.height = height
         self.pixels = [[color(0, 0, 0)] * height] * width
+    
+    def write_pixel(self, x: int, y: int, c: color) -> None:
+        self.pixels[x][y] = c
+    
+    def pixel_at(self, x: int, y: int) -> color:
+        return self.pixels[x][y]
--- src/canvas/tests/test_canvas.py
+++ src/canvas/tests/test_canvas.py
@@ -8,3 +8,10 @@ def test_canvas_creation():
     for row in c.pixels:
         for pixel in row:
             assert pixel == color(0, 0, 0)
+
+def test_canvas_write_pixel():
+    c = canvas(10, 20)
+    red = color(1, 0, 0)
+    c.write_pixel(2, 3, red)
+    assert c.pixel_at(2, 3) == red
+    assert c.pixel_at(0, 0) == color(0, 0, 0)
