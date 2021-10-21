7dd36f3a9996c4e282f0f5f554aa499eb8c76114
Harsh Thakar <harshthakar002@gmail.com>
2021-10-22 00:32:47 +0100

    [Canvas] Fixing height and width flip in canvas

--- src/canvas/canvas.py
+++ src/canvas/canvas.py
@@ -4,10 +4,14 @@ class canvas():
     def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
-        self.pixels = [[color(0, 0, 0)] * height] * width
+        self.pixels = []
+        for i in range(height):
+            self.pixels.append([])
+            for j in range(width):
+                self.pixels[i].append(color(0, 0, 0))
     
     def write_pixel(self, x: int, y: int, c: color) -> None:
-        self.pixels[x][y] = c
+        self.pixels[y][x] = c
     
     def pixel_at(self, x: int, y: int) -> color:
-        return self.pixels[x][y]
+        return self.pixels[y][x]
