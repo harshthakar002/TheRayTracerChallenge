c363157527e9ef8b5c19bb6823190f146c104c50
Harsh Thakar <harshthakar002@gmail.com>
2021-10-23 21:55:39 +0100

    [Transform] Adding scaling

--- src/transformations/tests/test_transformer.py
+++ src/transformations/tests/test_transformer.py
@@ -19,3 +19,19 @@ def test_vector_translation():
     transform = Transformer.translation(5, -3, 2)
     v = Vector(-3, 4, 5)
     assert transform.multiply_matrix_and_tuple(v) == v
+
+def test_point_scaling():
+    transform = Transformer.scaling(2, 3, -4)
+    p = Point(-4, 6, 8)
+    assert transform.multiply_matrix_and_tuple(p) == Point(-8, 18, -32)
+
+def test_vector_scaling():
+    transform = Transformer.scaling(2, 3, 4)
+    v = Vector(-4, 6, 8)
+    assert transform.multiply_matrix_and_tuple(v) == Vector(-8, 18, 32)
+
+def test_vector_inverse_scaling():
+    transform = Transformer.scaling(2, 3, 4)
+    inverse_transform = MatrixInverter.invert(transform)
+    v = Vector(-4, 6, 8)
+    assert inverse_transform.multiply_matrix_and_tuple(v) == Vector(-2, 2, 2)
--- src/transformations/transformer.py
+++ src/transformations/transformer.py
@@ -9,3 +9,11 @@ class Transformer():
         translation_matrix.set(1, 3, y)
         translation_matrix.set(2, 3, z)
         return translation_matrix
+    
+    @staticmethod
+    def scaling(x: float, y: float, z: float) -> Matrix:
+        scaling_matrix = Matrix.generate_identity_matrix(4)
+        scaling_matrix.set(0, 0, x)
+        scaling_matrix.set(1, 1, y)
+        scaling_matrix.set(2, 2, z)
+        return scaling_matrix
