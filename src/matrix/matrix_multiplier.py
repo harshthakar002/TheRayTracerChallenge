870fc083eff67e2b6d7785310a39189bacc6851c
Harsh Thakar <harshthakar002@gmail.com>
2021-10-23 16:47:56 +0100

    [Matrix] Adding multiplcation for matrix and tuple

--- src/matrix/matrix_multiplier.py
+++ src/matrix/matrix_multiplier.py
@@ -1,4 +1,5 @@
 from matrix.matrix import Matrix
+from features.tuple import Tuple
 
 class MatrixMultiplier():
 
@@ -13,3 +14,10 @@ class MatrixMultiplier():
                     c += m1.get(i, j) * m2.get(j, k)
                 result[i].append(c)
         return Matrix.initialize_from_values(result)
+    
+    @staticmethod
+    def multiply_matrix_and_tuple(m: Matrix, t: Tuple) -> Tuple:
+        return Tuple(m.get(0, 0) * t.x + m.get(0, 1) * t.y + m.get(0, 2) * t.z + m.get(0, 3) * t.w,
+        m.get(1, 0) * t.x + m.get(1, 1) * t.y + m.get(1, 2) * t.z + m.get(1, 3) * t.w,
+        m.get(2, 0) * t.x + m.get(2, 1) * t.y + m.get(2, 2) * t.z + m.get(2, 3) * t.w,
+        m.get(3, 0) * t.x + m.get(3, 1) * t.y + m.get(3, 2) * t.z + m.get(3, 3) * t.w)
--- src/matrix/tests/test_matrix_multiplier.py
+++ src/matrix/tests/test_matrix_multiplier.py
@@ -1,8 +1,15 @@
 from matrix.matrix import Matrix
 from matrix.matrix_multiplier import MatrixMultiplier
+from features.tuple import Tuple
 
 def test_matrix_multiplication():
     a = Matrix.initialize_from_values([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])
     b = Matrix.initialize_from_values([[-2, 1, 2, 3], [3, 2, 1, -1], [4, 3, 6, 5], [1, 2, 7, 8]])
     c = Matrix.initialize_from_values([[20, 22, 50, 48], [44, 54, 114, 108], [40, 58, 110, 102], [16, 26, 46, 42]])
     assert MatrixMultiplier.multiply_matrices(a, b) == c
+
+def test_matrix_multiplication_with_tuple():
+    m = Matrix.initialize_from_values([[1, 2, 3, 4], [2, 4, 4, 2], [8, 6, 4, 1], [0, 0, 0, 1]])
+    t = Tuple(1, 2, 3, 1)
+    expected_result = Tuple(18, 24, 33, 1)
+    assert MatrixMultiplier.multiply_matrix_and_tuple(m, t) == expected_result
