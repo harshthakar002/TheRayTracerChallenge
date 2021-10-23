e50b4d0c36d07b25bd0574bef0bd5ac74c9a6eee
Harsh Thakar <harshthakar002@gmail.com>
2021-10-23 17:41:48 +0100

    [Matrix] Writing function to generate submatrix

--- src/matrix/matrix_inverter.py
+++ src/matrix/matrix_inverter.py
@@ -4,4 +4,13 @@ class MatrixInverter():
 
     @staticmethod
     def calculate_determinant_for_2_x_2_matrix(m: Matrix) -> float:
-        return (m.get(0, 0) * m.get(1, 1)) - (m.get(0, 1) * m.get(1, 0)) 
+        return (m.get(0, 0) * m.get(1, 1)) - (m.get(0, 1) * m.get(1, 0))
+
+    @staticmethod
+    def create_sub_matrix(m: Matrix, ignored_row: int, ignored_column: int) -> Matrix:
+        sub_matrix_rows = []
+        for i in range(m.row_count):
+            if i == ignored_row:
+                continue
+            sub_matrix_rows.append(m.data[i][:ignored_column] + m.data[i][ignored_column + 1:])
+        return Matrix.initialize_from_values(sub_matrix_rows)
--- src/matrix/tests/test_matrix_inverter.py
+++ src/matrix/tests/test_matrix_inverter.py
@@ -5,3 +5,19 @@ from features.equality import is_approximately_equal
 def test_determinant_for_2_x_2_matrix():
     m = Matrix.initialize_from_values([[1, 5], [-3, 2]])
     assert is_approximately_equal(MatrixInverter.calculate_determinant_for_2_x_2_matrix(m), 17)
+
+def test_sub_matrix_of_3_x_3_matrix():
+    m = Matrix.initialize_from_values([[1, 5, 0], [-3, 2, 7], [0, 6, -3]])
+    sub_matrix = MatrixInverter.create_sub_matrix(m, 0, 2)
+    excepted_result = m.initialize_from_values([[-3, 2], [0, 6]])
+    assert sub_matrix.row_count == 2
+    assert sub_matrix.column_count == 2
+    assert sub_matrix == excepted_result
+
+def test_sub_matrix_of_4_x_4_matrix():
+    m = Matrix.initialize_from_values([[-6, 1, 1, 6], [-8, 5, 8, 6], [-1, 0, 8, 2], [-7, 1, -1, 1]])
+    expected_result = Matrix.initialize_from_values([[-6, 1, 6], [-8, 8, 6], [-7, -1, 1]])
+    sub_matrix = MatrixInverter.create_sub_matrix(m, 2, 1)
+    assert sub_matrix.row_count == 3
+    assert sub_matrix.column_count == 3
+    assert sub_matrix == expected_result
