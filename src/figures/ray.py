218b33ec361417eff8e80a7cb3828861cea8c89e
Harsh Thakar <harshthakar002@gmail.com>
2021-10-24 11:52:53 +0100

    [Ray] Adding ray from a disatance

--- src/figures/ray.py
+++ src/figures/ray.py
@@ -6,3 +6,6 @@ class Ray():
     def __init__(self, origin: Point, direction: Vector) -> None:
         self.origin = origin
         self.direction = direction
+
+    def position(self, time: float) -> Point:
+        return self.origin + (self.direction * time)
--- src/figures/tests/test_ray.py
+++ src/figures/tests/test_ray.py
@@ -8,3 +8,10 @@ def test_ray_creation():
     ray = Ray(origin, direction)
     assert ray.origin == origin
     assert ray.direction == direction
+
+def test_compute_point_from_a_distance():
+    ray = Ray(Point(2, 3, 4), Vector(1, 0, 0))
+    assert ray.position(0) == Point(2, 3, 4)
+    assert ray.position(1) == Point(3, 3, 4)
+    assert ray.position(-1) == Point(1, 3, 4)
+    assert ray.position(2.5) == Point(4.5, 3, 4)
