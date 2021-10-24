260ea5f57d16222ddc54f83ebf4483d2c8d5d1ae
Harsh Thakar <harshthakar002@gmail.com>
2021-10-24 22:18:24 +0100

    [CodeQuality] Adding typing

--- src/physical/material.py
+++ src/physical/material.py
@@ -4,7 +4,7 @@ from features.equality import is_approximately_equal
 
 class Material():
 
-    def __init__(self, color=Color(1, 1, 1), ambient=0.1, diffuse=0.9, specular=0.9, shininess=200.0) -> None:
+    def __init__(self, color: Color=Color(1, 1, 1), ambient:float=0.1, diffuse:float=0.9, specular:float=0.9, shininess:float=200.0) -> None:
         self.color = color
         self.ambient = ambient
         self.diffuse = diffuse
