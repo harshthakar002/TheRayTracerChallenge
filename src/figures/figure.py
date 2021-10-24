c917c5aeb7e8c325348c189fdc0a65861c6e4331
Harsh Thakar <harshthakar002@gmail.com>
2021-10-24 12:53:09 +0100

    [Figures] Abstractiong intersection distance to base

--- src/figures/figure.py
+++ src/figures/figure.py
@@ -1,6 +1,11 @@
 from uuid import uuid4
+from figures.ray import Ray
+from typing import List
 
 class Figure():
 
     def __init__(self):
         self.id = uuid4().int
+    
+    def ray_intersection_distance(self, ray: Ray) -> List[float]:
+        raise NotImplementedError('Abstract method')
