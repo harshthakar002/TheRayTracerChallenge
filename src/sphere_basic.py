e46d55966207dcfe893ae9c902427d74d26a8aef
Harsh Thakar <harshthakar002@gmail.com>
2021-10-24 23:06:55 +0100

    [PuttingItTogether] Pushing sphere shadow update

--- src/sphere_basic.py
+++ src/sphere_basic.py
@@ -4,25 +4,32 @@ from canvas.canvas import Canvas
 from features.point import Point
 from features.vector import Vector
 from features.color import Color
+from physical.lighter import Lighter
 from transformations.figure_transformer import FigureTransformer
 from figures.intersection import Intersection
 from canvas.ppm_writer import PPMWriter
+from physical.point_light import PointLight
 
 s = Sphere()
-origin_t, direction_t = FigureTransformer.scaling(20, 20, 20)
+origin_t, direction_t = FigureTransformer.scaling(100, 100, 100)
 s.set_transform(origin_t, direction_t)
 origin_t, direction_t = FigureTransformer.translation(0, 0, 40)
 s.set_transform(origin_t, direction_t)
+s.material.color = Color(1, 0.2, 1)
+
+light = PointLight(Color(1, 1, 1), Point(-10, 10, -10))
 
 c = Canvas(500, 500)
 for i in range(-250, 250):
     for j in range(-250, 250):
-        ray = Ray(Point(0, 0, 0), Vector(i, j, 100).normalize())
+        ray = Ray(Point(0, 0, -5), Vector(i, j, 10).normalize())
         intersections = Intersection.find_intersections_of_ray_and_figure(ray, s)
-        hits = Intersection.calculate_hit(intersections)
-        if len(hits) != 0:
-            c.write_pixel(i + 250, j + 250, Color(0.8, 0.2, 0.4))
-        else:
+        hit = Intersection.calculate_hit(intersections)
+        if hit != None:
+            point = ray.position (hit.t)
+            normal = hit.object.normal_at(point)
+            eye = ray.direction.negate()
+            color = Lighter.lighting(hit.object.material, light, point, eye, normal)
             c.write_pixel(i + 250, j + 250, Color(1, 1, 1))
 
 ppm = PPMWriter.write_ppm_from_canvas(c)
