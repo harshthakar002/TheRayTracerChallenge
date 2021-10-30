039e82c81e2e254a468c176eb596a7cb584ff68b
Harsh Thakar <harshthakar002@gmail.com>
2021-10-30 16:53:57 +0100

    [Light] Fixing tests

--- src/physical/point_light.py
+++ src/physical/point_light.py
@@ -5,4 +5,4 @@ from physical.light import Light
 class PointLight(Light):
 
     def __init__(self, intensity: Color, position: Point) -> None:
-        super.__init__(intensity, position)
+        super().__init__(intensity, position)
