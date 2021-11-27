6d66f6a334ad0a9509a0802ab69339b72bc3c526
Harsh Thakar <harshthakar002@gmail.com>
2021-11-27 00:08:43 +0000

    [Cone] Adding a cone shape

---
+++ src/figures/cone.py
@@ -0,0 +1,58 @@
+from typing import List
+from figures.shape import Shape
+from figures.ray import Ray
+from math import sqrt, inf
+from features.equality import is_approximately_equal
+
+class Cone(Shape):
+
+    def __init__(self, minimum: float = -inf, maximum: float = inf, closed: bool = False):
+        self.minimum = minimum
+        self.maximum = maximum
+        self.closed = closed
+        super().__init__()
+    
+    def local_intersection_distance(self, ray: Ray) -> List[float]:
+        a = (ray.direction.x * ray.direction.x) - (ray.direction.y * ray.direction.y) + (ray.direction.z * ray.direction.z)
+        b = (2 * ray.origin.x * ray.direction.x) - (2 * ray.origin.y * ray.direction.y) + (2 * ray.origin.z * ray.direction.z)
+        c = (ray.origin.x * ray.origin.x) - (ray.origin.y * ray.origin.y) + (ray.origin.z * ray.origin.z)
+        if is_approximately_equal(a, 0.0):
+            if is_approximately_equal(b, 0.0):
+                return self.intersect_caps(ray, [])
+            else:
+                return self.intersect_caps(ray, [- c / (2 * b)])
+        disc = (b * b) - (4 * a * c)
+        if disc < 0:
+            return []
+        
+        t0 = (-b - sqrt(disc)) / (2 * a)
+        t1 = (-b + sqrt(disc)) / (2 * a)
+        if t0 > t1:
+            t0, t1 = t1, t0
+        ts = []
+        y0 = ray.origin.y + (t0 * ray.direction.y)
+        if self.minimum < y0 and y0 < self.maximum:
+            ts.append(t0)
+        
+        y1 = ray.origin.y + (t1 * ray.direction.y)
+        if self.minimum < y1 and y1 < self.maximum:
+            ts.append(t1)
+        return self.intersect_caps(ray, ts)
+    
+    @staticmethod
+    def check_cap(ray: Ray, t: float):
+        x = ray.origin.x + (t * ray.direction.x)
+        z = ray.origin.z + (t * ray.direction.z)
+        return ((x * x) + (z * z)) <= 1
+    
+    def intersect_caps(self, ray: Ray, ts: List[float]) -> List[float]:
+        if not self.closed or is_approximately_equal(ray.direction.y, 0):
+            return ts
+        t = (self.minimum - ray.origin.y) / ray.direction.y
+        if Cone.check_cap(ray, t):
+            ts.append(t)
+        
+        t = (self.maximum - ray.origin.y) / ray.direction.y
+        if Cone.check_cap(ray, t):
+            ts.append(t)
+        return ts
---
+++ src/figures/tests/test_cone.py
@@ -0,0 +1,31 @@
+from figures.cone import Cone
+from figures.ray import Ray
+from features.point import Point
+from features.vector import Vector
+from features.equality import is_approximately_equal
+from figures.intersection import Intersection
+
+def test_intersecting_cone_with_ray():
+    shape = Cone()
+    ray = Ray(Point(0, 0, -5), Vector(0, 0, 1).normalize())
+    xs = Intersection.find_intersections_of_ray_and_figure(ray, shape)
+    assert len(xs) == 2
+    assert is_approximately_equal(xs[0].t, 5)
+    assert is_approximately_equal(xs[1].t, 5)
+    ray = Ray(Point(0, 0, -5), Vector(1, 1, 1).normalize())
+    xs = Intersection.find_intersections_of_ray_and_figure(ray, shape)
+    assert len(xs) == 2
+    assert is_approximately_equal(xs[0].t, 8.66025)
+    assert is_approximately_equal(xs[1].t, 8.66025)
+    ray = Ray(Point(1, 1, -5), Vector(-0.5, -1, 1).normalize())
+    xs = Intersection.find_intersections_of_ray_and_figure(ray, shape)
+    assert len(xs) == 2
+    assert is_approximately_equal(xs[0].t, 4.55006)
+    assert is_approximately_equal(xs[1].t, 49.44994)
+
+def test_intersecting_cone_with_ray_parallel_to_one_of_the_halves():
+    shape = Cone()
+    r = Ray(Point(0, 0, -1), Vector(0, 1, 1).normalize())
+    xs = Intersection.find_intersections_of_ray_and_figure(r, shape)
+    assert len(xs) == 1
+    assert is_approximately_equal(xs[0].t, 0.35355)
