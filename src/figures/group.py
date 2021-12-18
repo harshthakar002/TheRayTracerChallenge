564d0099d32ca96ca60073101d647626ade2b72f
Harsh Thakar <harshthakar002@gmail.com>
2021-12-18 11:41:55 +0000

    [Group] Adding child to groups

--- src/figures/group.py
+++ src/figures/group.py
@@ -8,3 +8,10 @@ class Group(Shape):
 
     def is_empty(self) -> bool:
         return len(self.shapes) == 0
+    
+    def contains(self, shape: Shape) -> bool:
+        return shape in self.shapes
+
+    def add_child(self, shape: Shape) -> None:
+        self.shapes.append(shape)
+        shape.parent = self
--- src/figures/tests/test_group.py
+++ src/figures/tests/test_group.py
@@ -1,7 +1,16 @@
 from figures.group import Group
+from figures.shape import Shape
 from matrix.matrix import Matrix
 
 def test_create_new_group():
     g = Group()
     assert g.transform == Matrix.generate_identity_matrix(4)
     assert g.is_empty()
+
+def test_add_child_to_a_group():
+    g = Group()
+    s = Shape()
+    g.add_child(s)
+    assert not g.is_empty()
+    assert g.contains(s)
+    assert s.parent == g
