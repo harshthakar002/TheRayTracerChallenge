d8b8fdc464e85429dc7866b790c619fe7a9164a7
Harsh Thakar <harshthakar002@gmail.com>
2021-12-23 13:10:52 +0000

    [Bounds] Adding transformation for bounds

--- src/features/bounds.py
+++ src/features/bounds.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
+from typing import List
 from features.point import Point
+from features.tuple import Tuple
+from matrix.matrix import Matrix
+from math import inf
 
 class Bounds():
 
@@ -11,3 +15,38 @@ class Bounds():
     
     def __eq__(self, bounds: Bounds) -> bool:
         return self.min_point == bounds.min_point and self.max_point == bounds.max_point
+    
+    def transform(self, transformation_matrix: Matrix) -> Bounds:
+        transformed_vertices: List[Tuple] = []
+        ## we need to trandform each point of the cube individually and find a bounding box of the new transofrmed cube
+        transformed_vertices.append(transformation_matrix.multiply_matrix_and_tuple(Point(self.min_point.x, self.min_point.y, self.min_point.z)))
+        transformed_vertices.append(transformation_matrix.multiply_matrix_and_tuple(Point(self.min_point.x, self.min_point.y, self.max_point.z)))
+        transformed_vertices.append(transformation_matrix.multiply_matrix_and_tuple(Point(self.min_point.x, self.max_point.y, self.min_point.z)))
+        transformed_vertices.append(transformation_matrix.multiply_matrix_and_tuple(Point(self.min_point.x, self.max_point.y, self.max_point.z)))
+        transformed_vertices.append(transformation_matrix.multiply_matrix_and_tuple(Point(self.max_point.x, self.min_point.y, self.min_point.z)))
+        transformed_vertices.append(transformation_matrix.multiply_matrix_and_tuple(Point(self.max_point.x, self.min_point.y, self.max_point.z)))
+        transformed_vertices.append(transformation_matrix.multiply_matrix_and_tuple(Point(self.max_point.x, self.max_point.y, self.min_point.z)))
+        transformed_vertices.append(transformation_matrix.multiply_matrix_and_tuple(Point(self.max_point.x, self.max_point.y, self.max_point.z)))
+        min_x, min_y, min_z = inf, inf, inf
+        max_x, max_y, max_z = -inf, -inf, -inf
+        for transformed_vertex in transformed_vertices:
+            min_x = min(min_x, transformed_vertex.x)
+            min_y = min(min_y, transformed_vertex.y)
+            min_z = min(min_z, transformed_vertex.z)
+            max_x = max(max_x, transformed_vertex.x)
+            max_y = max(max_y, transformed_vertex.y)
+            max_z = max(max_z, transformed_vertex.z)
+        return Bounds(min_x, min_y, min_z, max_x, max_y, max_z)
+    
+    @staticmethod
+    def find_bounds_of_group_of_bounds(bounds_list: List[Bounds]) -> Bounds:
+        min_x, min_y, min_z = inf, inf, inf
+        max_x, max_y, max_z = -inf, -inf, -inf
+        for bounds in bounds_list:
+            min_x = min(min_x, bounds.min_point.x)
+            min_y = min(min_y, bounds.min_point.y)
+            min_z = min(min_z, bounds.min_point.z)
+            max_x = max(max_x, bounds.max_point.x)
+            max_y = max(max_y, bounds.max_point.y)
+            max_z = max(max_z, bounds.max_point.z)
+        return Bounds(min_x, min_y, min_z, max_x, max_y, max_z)
--- src/features/tests/test_bounds.py
+++ src/features/tests/test_bounds.py
@@ -1,7 +1,18 @@
 from features.point import Point
 from features.bounds import Bounds
+from transformations.transformer import Transformer
+from math import pi
 
 def test_bounds_creation():
     b = Bounds(1, 2, 3, 4, 5, 6)
     assert b.min_point == Point(1, 2, 3)
     assert b.max_point == Point(4, 5, 6) 
+
+def test_bounds_transform():
+    b = Bounds(-1, -1, -1, 1, 1, 1)
+    scaling = Transformer.scaling(1, 2, 1)
+    rotation = Transformer.rotation_y(pi / 4)
+    translation = Transformer.translation(2, 0 , 0)
+    transformation = scaling.multiply_matrices(rotation).multiply_matrices(translation)
+    new_b = b.transform(transformation)
+    assert new_b == Bounds(0, -2, -2.82842, 2.82842, 2, 0)
