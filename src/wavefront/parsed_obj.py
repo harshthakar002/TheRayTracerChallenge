6c4588667b4252b097df6f0535d86310cc0c078a
Harsh Thakar <harshthakar002@gmail.com>
2021-12-24 12:46:05 +0000

    [ObjParser] Adding vertex to parsed obj

--- src/wavefront/parsed_obj.py
+++ src/wavefront/parsed_obj.py
@@ -1,9 +1,18 @@
+from features.point import Point
+from math import inf
+
+
 class ParsedObj():
 
     def __init__(self) -> None:
         self.processed = 0
         self.ignored = 0
+        self.vertices = [Point(-inf, -inf, -inf)]
     
 
     def ignore(self) -> None:
         self.ignored += 1
+
+    def add_vertex(self, x: float, y: float, z: float) -> None:
+        self.vertices.append(Point(x, y, z))
+        self.processed += 1    
--- src/wavefront/tests/test_parsed_obj.py
+++ src/wavefront/tests/test_parsed_obj.py
@@ -1,3 +1,4 @@
+from features.point import Point
 from wavefront.parsed_obj import ParsedObj
 
 def test_parsed_obj_creation():
@@ -11,3 +12,11 @@ def test_parsed_obj_ignore():
     assert parsed_obj.ignored == 1
     parsed_obj.ignore()
     assert parsed_obj.ignored == 2
+
+def test_parsed_obj_add_vertex():
+    parsed_obj = ParsedObj()
+    parsed_obj.add_vertex(1, 2, 3)
+    parsed_obj.add_vertex(2, 3, 4)
+    assert parsed_obj.processed == 2
+    assert parsed_obj.vertices[1] == Point(1, 2, 3)
+    assert parsed_obj.vertices[2] == Point(2, 3, 4)
