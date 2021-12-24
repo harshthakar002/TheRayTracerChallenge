b4279f44d10cb59132661cf50183913a51116b7e
Harsh Thakar <harshthakar002@gmail.com>
2021-12-24 12:26:55 +0000

    [ObjParser] Adding ignore for parsed obj

--- src/wavefront/parsed_obj.py
+++ src/wavefront/parsed_obj.py
@@ -3,3 +3,7 @@ class ParsedObj():
     def __init__(self) -> None:
         self.processed = 0
         self.ignored = 0
+    
+
+    def ignore(self) -> None:
+        self.ignored += 1
--- src/wavefront/tests/test_parsed_obj.py
+++ src/wavefront/tests/test_parsed_obj.py
@@ -4,3 +4,10 @@ def test_parsed_obj_creation():
     parsed_obj = ParsedObj()
     assert parsed_obj.processed == 0
     assert parsed_obj.ignored == 0
+
+def test_parsed_obj_ignore():
+    parsed_obj = ParsedObj()
+    parsed_obj.ignore()
+    assert parsed_obj.ignored == 1
+    parsed_obj.ignore()
+    assert parsed_obj.ignored == 2
