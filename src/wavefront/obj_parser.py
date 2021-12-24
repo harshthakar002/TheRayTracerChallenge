17f88f72ec6de52ec254280bd639c503d662c17c
Harsh Thakar <harshthakar002@gmail.com>
2021-12-24 12:39:51 +0000

    [ObjParser] Adding case when parser can't parse a line

--- src/wavefront/obj_parser.py
+++ src/wavefront/obj_parser.py
@@ -1,4 +1,22 @@
+from wavefront.parsed_obj import ParsedObj
+
+
 class ObjParser():
 
     def __init__(self, filename: str) -> None:
+        if filename == None:
+            raise ValueError('Filename cannot be none')
         self.filename = filename
+    
+    def parse(self) -> ParsedObj: 
+        if self.filename == None:
+            raise ValueError('Filename cannot be none')
+        parsed_obj = ParsedObj()
+        with open(self.filename, 'r') as obj_file:
+            for line in obj_file:
+                self.parse_line(line, parsed_obj)
+        
+        return parsed_obj
+
+    def parse_line(self, line: str, parsed_obj: ParsedObj) -> None:
+        parsed_obj.ignore()        
--- src/wavefront/tests/test_obj_parser.py
+++ src/wavefront/tests/test_obj_parser.py
@@ -3,3 +3,8 @@ from wavefront.obj_parser import ObjParser
 def test_obj_parser_creation():
     obj_parser = ObjParser('test.obj')
     assert obj_parser.filename == 'test.obj'
+
+def test_obj_parser_gibberish():
+    obj_parser = ObjParser('src\\wavefront\\tests\\test_files\\gibberish.obj')
+    parsed_obj = obj_parser.parse()
+    assert parsed_obj.ignored == 5
