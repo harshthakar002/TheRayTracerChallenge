c3c7d0f67157120af57fac5ad16819ba9d589f23
Harsh Thakar <harshthakar002@gmail.com>
2021-12-25 00:42:41 +0000

    [Perf] Adding perf logging for renderer

--- src/physical/renderer.py
+++ src/physical/renderer.py
@@ -3,6 +3,7 @@ from physical.world import World
 from canvas.canvas import Canvas
 from physical.shader import Shader
 from figures.ray import Ray
+from datetime import datetime
 
 class Renderer():
 
@@ -10,8 +11,10 @@ class Renderer():
     def render(camera: Camera, world: World, depth: int = 1) -> Canvas:
         image = Canvas(camera.hsize, camera.vsize)
         for y in range(camera.vsize):
+            start = datetime.now()
             for x in range(camera.hsize):
                 ray = Ray.ray_for_pixel(camera, x, y)
                 color = Shader.color_at(world, ray, depth)
                 image.write_pixel(x, y, color)
+            print(str(y) + ': ' + str(datetime.now() - start))
         return image
