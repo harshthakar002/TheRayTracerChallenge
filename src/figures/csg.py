4308d9aa8699ba8fd5162875bfec8d8635abfeb7
Harsh Thakar <harshthakar002@gmail.com>
2021-12-31 18:12:07 +0000

    [CSG] Adding bounds

--- src/figures/csg.py
+++ src/figures/csg.py
@@ -1,5 +1,9 @@
 from enum import Enum, unique
 from typing import List
+from features.bounds import Bounds
+from features.point import Point
+from features.vector import Vector
+from figures.group import Group
 from figures.ray import Ray
 from figures.shape import Shape
 
@@ -19,6 +23,7 @@ class CSG(Shape):
         left.parent = self
         self.right = right
         right.parent = self
+        self.memoized_bounds = Bounds.find_bounds_of_group_of_bounds([self.left.bounds(), self.right.bounds()])
     
     def filter_intersections(self, intersections: List[tuple[float, Shape, float, float]]):
         is_in_left, is_in_right = False, False
@@ -41,6 +46,24 @@ class CSG(Shape):
         xs = self.left.local_intersect(transformed_ray) + self.right.local_intersect(transformed_ray)
         xs = sorted(xs, key=lambda intersection: intersection[0])
         return self.filter_intersections(xs)
+    
+    def is_out_of_bounds(self, ray: Ray) -> bool:
+        bounds = self.bounds()
+        xtmin, xtmax = Group.check_axis(bounds.min_point.x, bounds.max_point.x, ray.origin.x, ray.direction.x)
+        ytmin, ytmax = Group.check_axis(bounds.min_point.y, bounds.max_point.y, ray.origin.y, ray.direction.y)
+        ztmin, ztmax = Group.check_axis(bounds.min_point.z, bounds.max_point.z, ray.origin.z, ray.direction.z)
+        tmin = max(xtmin, ytmin, ztmin)
+        tmax = min(xtmax, ytmax, ztmax)
+        return tmin > tmax
+    
+    def memoize_bounds(self) -> None:
+        self.memoized_bounds = Bounds.find_bounds_of_group_of_bounds([self.left.bounds(), self.right.bounds()])
+    
+    def normal_at(self, point: Point, u: float, v: float) -> Vector:
+        raise NotImplementedError('This method should not be called for groups')
+    
+    def bounds(self) -> Bounds:
+        return self.memoized_bounds
 
     @staticmethod
     def is_intersection_allowed(operation: CSGOperation, is_left_hit: bool, is_in_left: bool, is_in_right: bool) -> bool:
